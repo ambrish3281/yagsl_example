1a192bcbe9fed0586dc2084bad784b894cbbbd24
Vihaan Mehta <ambrishmehta@yahoo.com>
2025-03-27 16:56:58 -0700

    032725_2

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -23,13 +23,13 @@ public final class Constants
   // APRIL TAG CONSTANTS
 
   // Auto constants
-	public static final double X_REEF_ALIGNMENT_P = 1;
-	public static final double Y_REEF_ALIGNMENT_P = 1;
+	public static final double X_REEF_ALIGNMENT_P = 2;
+	public static final double Y_REEF_ALIGNMENT_P = 2;
 	public static final double ROT_REEF_ALIGNMENT_P = 0.058;
 
 	public static final double ROT_SETPOINT_REEF_ALIGNMENT = 0;  // Rotation
 	public static final double ROT_TOLERANCE_REEF_ALIGNMENT = 1;
-	public static final double X_SETPOINT_REEF_ALIGNMENT = -0.2;  // Vertical pose
+	public static final double X_SETPOINT_REEF_ALIGNMENT = -0.1;  // Vertical pose
 	public static final double X_TOLERANCE_REEF_ALIGNMENT = 0.02;
 	public static final double Y_SETPOINT_REEF_ALIGNMENT = 0.16;  // Horizontal pose
 	public static final double Y_TOLERANCE_REEF_ALIGNMENT = 0.02;
